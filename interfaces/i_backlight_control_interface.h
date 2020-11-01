6964534dcf94b21d65ba4abd933c3bacdbec21a5
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2020-11-01 17:59:19 +0400

    set_backlight_intencity returns bool

--- interfaces/i_backlight_control_interface.h
+++ interfaces/i_backlight_control_interface.h
@@ -9,7 +9,7 @@ namespace opensalad
 			class i_backlight_control_interface
 			{
 			public:
-				virtual void set_backlight_intencity(byte_t const& i) = 0;
+				virtual bool set_backlight_intencity(byte_t const& i) = 0;
 				virtual byte_t get_backlight_intencity() const = 0;
 
 				virtual bool set_backlight_color(
