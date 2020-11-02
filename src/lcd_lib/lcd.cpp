d3d4193a260066fecfef2d7e7a74c5ab894b7e98
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2020-11-02 23:24:26 +0400

    Simple implementation of lcd class

--- src/lcd_lib/lcd.cpp
+++ src/lcd_lib/lcd.cpp
@@ -1,9 +1,18 @@
+#include "stdafx.h"
 #include "lcd.h"
 
 namespace opensalad
 {
 	namespace lcd
 	{
+		void lcd::send_command(byte_t const& cmd)
+		{
 
+		}
+
+		void lcd::send_data(byte_t const& data)
+		{
+
+		}
 	}
 }
--- src/lcd_lib/lcd.h
+++ src/lcd_lib/lcd.h
@@ -1,9 +1,19 @@
 #pragma once
 
+#include <i_lcd_interface.h>
+#include "observable_backlight_control.h"
+
 namespace opensalad
 {
 	namespace lcd
 	{
-
+		class LCD_EXPORT lcd
+			: public iface::i_lcd_interface
+			, public observable_backlight_control
+		{
+		public:
+			void send_command(byte_t const& cmd) override;
+			void send_data(byte_t const& data) override;
+		};
 	}
 }
