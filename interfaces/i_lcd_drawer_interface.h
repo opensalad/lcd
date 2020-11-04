e41363563fcac83cd442bbb9de0c221652ddcaff
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2020-11-05 00:38:10 +0400

    Incomplete naming fixed

--- interfaces/i_lcd_drawer_interface.h
+++ interfaces/i_lcd_drawer_interface.h
@@ -16,7 +16,7 @@ namespace opensalad
 				virtual void set_pin_count(byte_t count) = 0;
 				virtual void set_pinout(std::vector<std::string> const& pinout) = 0;
 				virtual void set_dimension(dimensions_t screen_dimension) = 0;
-				virtual void set_backlight(byte_t backlight) = 0;
+				virtual void set_backlight_intencity(byte_t backlight_intencity) = 0;
 				virtual void set_backlight_color(color_t backlight_color) = 0;
 				virtual void set_special_character(byte_t character_code, character_t character) = 0;
 			};
